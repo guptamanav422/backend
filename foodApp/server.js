cd1d04ea7625ace5f7e408dd53d12d81d83aaff5
Manav <guptamanav422@gmail.com>
2021-09-16 16:11:01 +0530

    router in express

--- foodApp/server.js
+++ foodApp/server.js
@@ -1,6 +1,6 @@
 const express=require("express");
 const app=express();
-
+// const router=express.Router();
 app.listen('5000',function(){
     console.log("server listening on port 5000");
 })
@@ -9,34 +9,57 @@ app.listen('5000',function(){
 app.use(express.json());
 let user={};
 
-app.get("/",(req,res)=>{
-    res.send("home page");
-})
 
-app.get("/user",(req,res)=>{
-    res.json(user)
-})
+const userRouter=express.Router();
+app.use('/user',userRouter);
+// mouting in express 
+userRouter
+.route("/")
+.get(getUser)
+.post(postUser)
+.patch(updateUser)
+.delete(deleteUser)
 
-app.post("/user",(req,res)=>{
+userRouter
+.route('/:id')
+.get(getUserById)
+
+
+
+// app.get("/",(req,res)=>{
+//     res.send("home page");
+// })
+
+// app.get("/user",getUser)
+function getUser(req,res){
+    res.json(user);
+}
+
+// app.post("/user",postUser)
+function postUser(req,res){
     user=req.body;
-    console.log(req.body);
+    // console.log(req.body);
     res.send("data has been add successfully")
-})
+}
 
-app.patch("/user",(req,res)=>{
+// app.patch("/user",updateUser )
+function updateUser(req,res){
     let obj=req.body;
     for(let key in obj){
         user[key]=obj[key];
     }
     res.json(user)
-})
+}
 
-app.delete("/user",(req,res)=>{
+
+// app.delete("/user",deleteUser)
+function deleteUser(req,res){
     user={};
     res.json(user);
-})
+}
 
-app.get("/user/:id",(req,res)=>{
+// app.get("/user/:id",getUserById)
+function getUserById(req,res){
     console.log(req.params);
     res.send(req.params.id);
-})
+}
